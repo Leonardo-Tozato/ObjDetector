6d491c0ac444849a425a9a16e1b8bc2b288d4551
Abner Figueiredo Bertelline <abner.figueiredo.bertelline@gmail.com>
2017-10-06 18:19:02 +0000

    Rotate taking photos

--- detector_ua.py
+++ detector_ua.py
@@ -4,7 +4,6 @@ import cv2
 import serial
 import threading
 import os
-import time
 from socket import *
 from PIL import Image
 
@@ -13,32 +12,9 @@ classes = ["background", "aeroplane", "bicycle", "bird", "boat",
     "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
     "sofa", "train", "tvmonitor"]
 
-objects = {  "aviao": "aeroplane",
-                    "bicicleta": "bicycle",
-                    "passaro": "bird",
-                    "canoa": "boat",
-                    "garrafa": "bottle",
-                    "onibus": "bus",
-                    "carro": "car",
-                    "gato": "cat",
-                    "cadeira": "chair",
-                    "vaca": "cow",
-                    "mesa": "diningtable",
-                    "cachorro": "dog",
-                    "cavalo": "horse",
-                    "motocicleta": "motorbike", "moto": "motorbike",
-                    "pessoa": "person", "humano": "person",
-                    "planta": "pottedplant",
-                    "ovelha": "sheep",
-                    "sofa": "sofa",
-                    "trem": "train",
-                    "tv": "tvmonitor", "televisao": "tvmonitor" }
-
 colors = np.random.uniform(0, 255, size=(len(classes), 3))
-socket_num = 3001
+socket_num = 3000
 arduin_com = serial.Serial('/dev/ttyACM0', 9600)
-msecs_per_degree = 1
-msecs_per_unit = 10
 
 def predict(image, net):
     (h, w) = image.shape[:2]
@@ -69,28 +45,20 @@ def capture_predict():
     cv2.imshow("Output", output)
     cv2.waitKey(0)
 
-# rotate 300ms right per frame
-# 4500ms = ~360dg
-def search(obj):
-    print 'searching for a', obj
+def round_msecs(msecs):
+    return min(9, msecs//500)
 
 def forward(msecs):
     arduin_com.write('1')
     arduin_com.write(str(round_msecs(msecs)))
 
 def turn_right(msecs):
-    units = int(round(float(msecs) / msecs_per_unit))
-    while units > 0:
-        arduin_com.write('2' + str(min(units-1,9)))
-        units -= min(units, 10)
-    time.sleep((msecs * 1.2) / 1000.0)
+    arduin_com.write('2')
+    arduin_com.write(str(round_msecs(msecs)))
 
 def turn_left(msecs):
-    units = int(round(float(msecs) / msecs_per_unit))
-    while units > 0:
-        arduin_com.write('3' + str(min(units-1,9)))
-        units -= min(units, 10)
-    time.sleep((msecs * 1.2) / 1000.0)
+    arduin_com.write('3')
+    arduin_com.write(str(round_msecs(msecs)))
 
 def backward(msecs):
     arduin_com.write('4')
@@ -104,27 +72,27 @@ def receive(rcv_socket):
     data = rcv_socket.recv(2048)
     cmd = list(str(data.decode('utf-8')).lower())
     cmd = ''.join([c for c in cmd if c != '\x00'])
-    words = cmd.split(' ')
-    if 'encontre' in words or 'pegue' in words:
-        obj = words[2]
-        if obj in objects:
-            search(objects[obj])
+    print (cmd)
+    if cmd == 'taca-le pau' or cmd == 'taca-lhe pau' or cmd == 'tacale pau':
+        forward(500)
+    elif cmd == 'vire para direita':
+    	turn_right(1000)
+    elif cmd == 'vire para esquerda':
+	turn_left(1000)
+    elif cmd == 'va para tras':
+   	backward(1000)
+    elif cmd == 'faca barulho':
+        beep(3)
+    elif cmd == 'mostre todas as funcionalidades':
+        forward(1000)
+	turn_left(1000)
+	turn_right(1000)
+	backward(1000)
+        beep(3)
     rcv_socket.send('ack'.encode(encoding='utf-8', errors='ignore'))
     rcv_socket.close()
 
-for i in range(0, 13):
-    os.system("fswebcam -r 299x299 --jpeg 85 -S 10 img"+str(i)+".jpg")
-    turn_right(300)
-
-'''
-while True:
-    msecs = int(raw_input())
-    if msecs > 0:    
-        turn_left(msecs)
-    else:
-        turn_right(msecs * -1)
-'''
-'''my_socket = socket(AF_INET,SOCK_STREAM)
+my_socket = socket(AF_INET,SOCK_STREAM)
 my_socket.bind(('',socket_num))
 my_socket.listen(1)
 
@@ -132,4 +100,4 @@ while True:
     rcv_socket, addr = my_socket.accept()
     new_thread = threading.Thread(target=receive, args=(rcv_socket,))
     new_thread.start()
-my_socket.close()'''
+my_socket.close()
